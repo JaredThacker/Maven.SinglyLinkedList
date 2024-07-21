6a252ac21662c13a9fbd348c391d9308051eb70a
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-07-20 22:52:24 -0400

    in progress

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -6,7 +6,6 @@ package com.zipcodewilmington.singlylinkedlist;
 public class SinglyLinkedList <E extends Comparable<E>> {
 
     public Node head;
-
     int size;
 
     public class Node {
@@ -19,15 +18,37 @@ public class SinglyLinkedList <E extends Comparable<E>> {
         }
     }
 
-
-
     public SinglyLinkedList() {
         head = null;
         size = 0;
     }
 
-    public void add(Node element){
-
+    public int size(){
+        return size;
     }
 
+    public void add(E element){
+        Node newNode = new Node(element);
+        if (head == null){
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public E get(int index){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("Index " + index + "out of bounds" + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+        return current.data;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,9 +1,39 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    SinglyLinkedList<Integer> list;
+
+    @Before
+    public void setUp(){
+        list = new SinglyLinkedList<>();
+    }
+
+    @Test
+    public void testAdd(){
+        list.add(2);
+        Integer expected = 2;
+        Assert.assertEquals(expected, list.get(0));
+    }
+
+    @Test
+    public void testGet(){
+        list.add(1);
+        list.add(2);
+        Integer expected = 2;
+        Assert.assertEquals(expected, list.get(1));
+    }
+
+    @Test
+    public void testSize(){
+        list.add(1);
+        Assert.assertEquals(1, list.size());
+    }
 }
